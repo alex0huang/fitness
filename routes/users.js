6d5fbad4bf174e34aeeddc90206ad4da24b34c4d
alex0huang <alexhuangjiasheng@gmail.com>
2025-11-08 16:02:53 -0800

    Add user login view and database schema

--- routes/users.js
+++ routes/users.js
@@ -27,13 +27,30 @@ router.post('/', async (req, res) => {
             res.redirect(`/users/${req.body.firstname}`);
         } catch (error) {
             console.log('Error hashing password:', error);
-            res.render('users/new', { firstname: req.body.firstname, error: '创建用户时发生错误' });
+             res.render('users/new', { firstname: req.body.firstname, error: '创建用户时发生错误' });
         }
     } else {
         console.log('Error');
         res.render('users/new', { firstname: req.body.firstname });
     } 
 });
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', async (req, res) => {
+    const user = users.find(user => user.firstname === req.body.firstname);
+    if(!user){
+        return res.render('users/login', { firstname: req.body.firstname, error: '用户不存' });
+    }
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    if(isPasswordValid){
+        res.send('登录成功');
+    } else {
+        return res.render('users/login', { firstname: req.body.firstname, error: '密码错误' });
+    }
+});
+
 //route chaining
 router.route('/:id')
     .get((req, res) => {
